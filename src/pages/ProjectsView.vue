c2be140eabaef5c3d63fb3c78db6ccb61a4f4e80
Nath <nsoraya.viscaino@gmail.com>
2024-10-21 17:19:45 +0200

    end day one

---
+++ src/pages/ProjectsView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects</h1>
+    <RouterLink to="/homeView">Home</RouterLink>
+  </div>
+</template>
---
+++ src/pages/SingleProjectView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Project {{ route.params?.id }}</h1>
+    <RouterLink to="/projectsView">Projects</RouterLink>
+    <br />
+  </div>
+</template>
