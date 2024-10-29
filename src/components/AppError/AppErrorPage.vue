0d33cbeeb44187cec275b53295214cc47ec5b6e0
Nathaly Viscaíno <166789478+Nathbecode@users.noreply.github.com>
2024-10-29 13:53:44 +0100

    Final commit

--- src/components/AppError/AppErrorPage.vue
+++ src/components/AppError/AppErrorPage.vue
@@ -1,26 +1,50 @@
 <script setup lang="ts">
 const router = useRouter()
 
+const errorStore = useErrorStore()
+
+const error = ref(errorStore.activeError)
+
+const message = ref('')
+const customCode = ref(0)
+const details = ref('')
+const code = ref('')
+const hint = ref('')
+const statusCode = ref(0)
+
+if (error.value && !('code' in error.value)) {
+  message.value = error.value.message
+  customCode.value = error.value.customCode ?? 0
+}
+
+if (error.value && 'code' in error.value) {
+  message.value = error.value.message
+  details.value = error.value.details
+  hint.value = error.value.hint
+  code.value = error.value.code
+  statusCode.value = error.value.statusCode ?? 0
+}
+
+const ErrorTemplate = import.meta.env.DEV
+  ? defineAsyncComponent(() => import('./AppErrorDevSection.vue'))
+  : defineAsyncComponent(() => import('./AppErrorProdSection.vue'))
+
 router.afterEach(() => {
-  useErrorStore().activeError = false
+  errorStore.clearError()
 })
 </script>
 
 <template>
   <section class="error">
-    <div>
-      <iconify-icon icon="lucide:triangle-alert" class="error__icon" />
-      <h1 class="error__code">404</h1>
-      <p class="error__msg">Page not found</p>
-      <div class="error-footer">
-        <p class="error-footer__text">
-          You'll find lots to explore on the home page.
-        </p>
-        <RouterLink to="/">
-          <Button class="max-w-36"> Back to homepage </Button>
-        </RouterLink>
-      </div>
-    </div>
+    <ErrorTemplate
+      :message
+      :customCode
+      :code
+      :statusCode
+      :hint
+      :details
+      :isCustomError="errorStore.isCustomError"
+    />
   </section>
 </template>
 
